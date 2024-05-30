6277dfda59e509f3184ff274a7a500d3b07a4e4f
Mariam2572 <mariamkamel791@gmail.com>
2024-05-31 01:35:57 +0300

    Add 'Best Seller' section to HomeView and create Styles utility class for text styles

--- lib/features/home/persentation/views/home_view.dart
+++ lib/features/home/persentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/persentation/views/widgets/book_list_view.dart';
 import 'package:bookly/features/home/persentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const CustomAppBar(),
           SizedBox(
            height: MediaQuery.of(context).size.height*.3,
 
-            child: const BookListView())
+            child: const BookListView(),),
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 25.w,vertical: 50.h),
+              child: const Text('Best Seller',style: Styles.titleMedium,),
+            )
         ],
       ),
     );
